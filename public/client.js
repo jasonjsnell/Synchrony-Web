a3c4c58046b3bbc33afc5e1fd5dba629e850e63b
jasonjsnell <snell.jason@gmail.com>
2022-11-02 11:21:57 -0400

    updated with glitch code updates

--- public/client.js
+++ public/client.js
@@ -1,54 +1,64 @@
+//socket connection
+let socket = io();
+
+//connect socket client 
+socket.on('connect', () => {
+  console.log("Socket: client.JS file in index.html connected")
+});
+
 window.addEventListener('load', () => {
-    
-    console.log('Window loaded');
 
-    let bpmHistoryDiv = document.getElementById('bpm-history');
+  console.log('Window loaded');
 
-    //RETRIEVING DATA FROM THE DATABASE
-    //fetch all messages from server
-    fetch('/bpm-history')
+  //div to list the DB entries in HTML
+  let bpmHistoryDiv = document.getElementById('bpm-history');
+
+  //RETRIEVING DATA FROM THE DATABASE
+  //fetch all messages from server
+  fetch('/bpm-history')
     .then(response => response.json())
     .then(data => {
-        
-        //get messages from data, and var name inside is data
-        let bpmHistoryFromDb = data.bpms;
-      
+
+      //get messages from data, and var name inside is data
+      let bpmHistoryFromDb = data.bpms;
+
       //sort by date, most recent first
-        bpmHistoryFromDb.sort(sortByDate);
-      
+      bpmHistoryFromDb.sort(sortByDate);
+
       //only list the most recent 100 entries so the HTML page doesn't explode
       let maxDisplay = bpmHistoryFromDb.length;
       if (maxDisplay > 100) {
         maxDisplay = 100;
       }
-      
-        //loop through each message individually
-        for (let i = 0; i < maxDisplay; i++){
-            
-            let bpmFromDb = bpmHistoryFromDb[i];
-            let bpm = bpmFromDb.bpm;
-            let time = bpmFromDb.time;
-            let user = bpmFromDb.user;
 
-          //if all the vars are valid...
-            if (user && bpm && time) {
-              
-              //print on screen
-              let newBpmDisplay = document.createElement('p');
-              let newBpmContent = `${bpm} BPM from User ${user} at ${time} : `;
-              newBpmDisplay.innerHTML = newBpmContent;
-              bpmHistoryDiv.appendChild(newBpmDisplay);
-            }
-            
+      //loop through each message individually
+      for (let i = 0; i < maxDisplay; i++) {
+
+        let bpmFromDb = bpmHistoryFromDb[i];
+        let bpm = bpmFromDb.bpm;
+        let time = bpmFromDb.time;
+        let user = bpmFromDb.user;
+
+        //if all the vars are valid...
+        if (user && bpm && time) {
+
+          //print on screen
+          let newBpmDisplay = document.createElement('p');
+          let newBpmContent = `${bpm} BPM from ${user} at ${time} : `;
+          newBpmDisplay.innerHTML = newBpmContent;
+          bpmHistoryDiv.appendChild(newBpmDisplay);
         }
+
+      }
     })
     .catch(error => {
-        console.log(error);
+      console.log(error);
     })
 
 })
 
+//HELPER SUBS
 //sorts JSON ojbect array by date
 function sortByDate(a, b) {
-    return new Date(b.time).getTime() - new Date(a.time).getTime();
+  return new Date(b.time).getTime() - new Date(a.time).getTime();
 }
--- server.js
+++ server.js
@@ -1,17 +1,78 @@
-//Server
-let express = require("express");
-let server = express();
+//Synchrony
+//setup
+let express = require('express');
+let app = express();
+app.use('/', express.static('public'));
 
-server.use("/", express.static("public")); //set home folder to find client side files
-server.use(express.json()); //let express know we are using JSON data
+//create http server (needed for socket)
+let server = require('http').createServer(app);
 
 //database
 let DataStore = require("nedb");
 let db = new DataStore("bpms.db");
 db.loadDatabase();
 
-//Routes
-server.get("/bpm-history", (request, response) => {
+//removal code
+// db.remove({}, { multi: true }, function (err, numRemoved) {
+//   console.log("DB reset: num removed:", numRemoved);
+// });
+
+
+//socket
+const { Server } = require('socket.io');
+const io = new Server(server);
+
+//enviroment port or 3000 port
+let port = process.env.PORT || 3000;
+
+server.listen(port, () => {
+    console.log("Server is listening at localhost:", + port);
+});
+
+//--> SOCKETS
+io.sockets.on('connection', (socket) => {
+    console.log("Socket: connected");
+  
+ 
+  socket.on('disconnect', (data) => {
+    console.log("Socket: disconnected");
+  });
+  
+
+  socket.on('newBpmFromWatch', (dataFromWatch) => {
+    
+    console.log("New BPM from watch", dataFromWatch);
+  
+    //replicate data object for DB insertion
+    let dataFromWatchForDb = dataFromWatch;
+    
+    //add timestamp to db storage obj 
+    dataFromWatchForDb.time = Date();
+
+    //save it to the database
+    db.insert(dataFromWatchForDb, (err, newDocs) => {
+      if (err) {
+        console.log("DB error");
+      } else {
+        console.log("DB success");
+      }
+    });
+    
+    //TODO: can a notification happen here to tell the client to refresh its feed?
+    
+    //in this build, send it back out so same watch can buzz at its local rate
+    //next version, it will send the bpmData to the other watch
+    io.sockets.emit('newBpmFromServer', {bpmData: dataFromWatch});
+    //send to others, and not self
+    //socket.broadcast.emit('mouseDrawData', data);
+  });
+  
+  
+});
+
+//--> ROUTES
+//Accessor to BPM history, called by HTML page
+app.get("/bpm-history", (request, response) => {
   //get data from DB
   db.find({}, (err, dbBpms) => {
     if (err) {
@@ -27,34 +88,28 @@ server.get("/bpm-history", (request, response) => {
   });
 });
 
-//RECEIVE NEW BPM UPDATES FROM WATCH
-server.post("/new-bpm", (request, response) => {
+//Version 2 
+//Setter: new BPM updates from watch
+// app.post("/new-bpm", (request, response) => {
   
-
-  //retrieve data obj
-  let dataFromWatch = request.body;
+//   //retrieve data obj
+//   let dataFromWatch = request.body;
   
-  console.log("data from watch", dataFromWatch);
+//   console.log("data from watch", dataFromWatch);
 
-  //add timestamp to it
-  dataFromWatch.time = Date();
+//   //add timestamp to it
+//   dataFromWatch.time = Date();
 
-  let dataObj = {
-    content: dataFromWatch,
-  };
+//   let dataObj = {
+//     content: dataFromWatch,
+//   };
 
-  //save it to the database
-  db.insert(dataFromWatch, (err, newDocs) => {
-    if (err) {
-      response.json({ task: "DB Error" });
-    } else {
-      response.json({ task: "DB Success" });
-      //response.json(dataObj);
-    }
-  });
-});
-
-let port = process.env.PORT || 3000;
-server.listen(port, () => {
-  console.log(`Server listening on localhost:${port}`);
-});
+//   //save it to the database
+//   db.insert(dataFromWatch, (err, newDocs) => {
+//     if (err) {
+//       response.json({ task: "DB Error" });
+//     } else {
+//       response.json({ task: "DB Success" });
+//     }
+//   });
+// });
